9409cc29d789ddb35c55d4c60a6495648bad6bc0
senchin228-star <senchin228@gmail.com>
2026-08-04 17:54:55 +0300

    feet: added text columns to hexdump

--- hex_dump3.c
+++ hex_dump3.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_hex_dump(const void *ptr, int shift) 
+void print_hex_dump(const char *ptr, int shift) 
 {
     unsigned char *p = (unsigned char *)ptr;
     size_t i = 0;
     printf("%08x  " ,shift * 16);
     for (i = 0; i < 16; i++) {
         printf("%02x ", *p);
+        if ( *p == '\n') *p = '.';
+        if ( *p == 0) *p = '.';
         p++;
     }
+    printf("[%s]", ptr);
     printf("\n");
 }
 int main()
