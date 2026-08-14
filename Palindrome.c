169fd04946716d757e59214fbd926be16f2f6b1e
senchin228-star <senchin228@gmail.com>
2026-08-14 17:56:40 +0300

    feet: palindrome

---
+++ Palindrome.c
@@ -0,0 +1,14 @@
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
+
+    long rev = 0;
+    int original = x;
+
+    while (x != 0) {
+        rev = rev * 10 + x % 10;
+        x = x / 10;
+    }
+
+    return original == rev;
+}
