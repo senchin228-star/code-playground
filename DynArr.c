4f8148369ef8cf1b912df55325c5f82b968c83a5
senchin228-star <senchin228@gmail.com>
2026-08-17 21:51:34 +0300

    feet: made dynamic array

---
+++ DynArr.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define START_CAPACITY 1
+typedef struct {
+    int *data;
+    size_t size;
+    size_t capacity;
+} DynArray;
+
+void da_init(DynArray *arr)
+{
+    arr->size = 0;
+    arr->capacity = START_CAPACITY;
+    arr->data = malloc(arr->capacity * sizeof(int));
+    if (arr->data == NULL) return; 
+}
+
+int da_push(DynArray *arr, int val)
+{
+    if (arr->size >= arr->capacity){
+        int *tmp = realloc(arr->data, arr->capacity * 2 * sizeof(int));
+        if (tmp == NULL) return 0;
+        arr->data = tmp;
+        arr->capacity = arr->capacity * 2;
+    }
+    arr->data[arr->size] = val;
+    arr->size++;
+    return 1;
+}
+int  da_get(DynArray *arr, size_t index, int *out)
+{
+    if (index >= arr->size) return 0;
+    *out = arr->data[index];
+    return 1;
+}
+void da_free(DynArray *arr)
+{
+    free(arr->data);
+    arr->data = NULL;
+}
+
