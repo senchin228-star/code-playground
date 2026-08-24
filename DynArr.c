2358e5e489cd3d0534b9cf5a70191975c4da7e0a
Arsenko <senchin228@gmail.com>
2026-08-24 15:10:17 +0300

    Fix da_random

--- DynArr.c
+++ DynArr.c
@@ -41,8 +41,8 @@ void da_free(DynArray *arr)
 void da_random(DynArray *arr) 
 {
     if (arr->size <= 0) return;
-    for (int i = 0; i < arr->size; i++){
-        int index = rand() % arr->size;
+    for (int i = 0; i < arr->size - 1; i++){
+        int index = i + rand() % arr->size - 1;
         int tmp = arr->data[index];
         arr->data[index] = arr->data[i];
         arr->data[i] = tmp;
