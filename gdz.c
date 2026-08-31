1647db8f781f567aee6d8d091c5d7ed7dedbeaaf
senchin228-star <senchin228@gmail.com>
2026-08-31 12:52:13 +0300

    feet: add Added answer keys for Russian and English; you can now specify exercise numbers (including the module and letter for English).

--- gdz.c
+++ gdz.c
@@ -4,14 +4,24 @@
 
 int main(int argc, char** argv)
 {
-    if (argc != 3) return 1;
-    const char mathurl[] = "https://gdz.ru/class-10/algebra/";
-    char command[512] = "pwd";
+    if (argc == 4 && strcmp(argv[1], "en" ) != 0){
+        if (argc != 3) return 1;
+    }
+    const char mathurl[] = "https://gdz.ru/class-10/algebra/alimov-15/";
+    const char rusurl[] = "https://reshak.ru/otvet/otvet14.php?otvet1=";
+    const char enurl[] = "https://reshak.ru/otvet/otvet_txt.php?otvet1=/spotlight10/images/";
+    char command[512] = "echo 'invalid argument'";
 
     if (strcmp(argv[1], "math") == 0){
-        snprintf(command, sizeof(command), "chromium %s", mathurl);
+        snprintf(command, sizeof(command), "xdg-open %s%s-nom/", mathurl, argv[2]);
     }
-    printf("argv[1] : %s, command: %s", argv[1], command);
+    if (strcmp(argv[1], "rus") == 0){
+        snprintf(command, sizeof(command), "xdg-open %s%s", rusurl, argv[2]);
+    }
+    if (strcmp(argv[1], "en") == 0){
+        snprintf(command, sizeof(command), "xdg-open %smodule%s/%s/1", enurl, argv[2], argv[3]);
+    }
+
     system(command);
     return 0;
 }
