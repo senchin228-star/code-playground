e6c4893383ed004e98716fea5996a0721177774c
senchin228-star <senchin228@gmail.com>
2026-09-01 16:49:54 +0300

    feet: add geometry and change addres for myself

--- gdz.c
+++ gdz.c
@@ -5,27 +5,33 @@
 
 int main(int argc, char** argv)
 {
-    if (argc < 2){
+    if (argc <= 2){
         printf("insufficient arguments");
         return 1;
     }
-    if (argc == 4 && strcmp(argv[1], "en" ) != 0){
-        if (argc != 3) return 1;
-    }
-    const char mathurl[] = "https://gdz.ru/class-10/algebra/alimov-15/";
-    const char rusurl[] = "https://reshak.ru/otvet/otvet14.php?otvet1=";
+
+    const char mathurl[] = "https://gdz.ru/class-10/algebra/merzlyak-nomirovskij-uglublennij/";
+    const char geomurl[] = "https://gdz.ru/class-10/geometria/atanasyan-10-11/10-class-";
+    const char rusurl[] = "https://reshak.ru/otvet/reshebniki.php?otvet=";
     const char enurl[] = "https://reshak.ru/otvet/otvet_txt.php?otvet1=/spotlight10/images/";
     char command[512];
 
-    if (strcmp(argv[1], "math") == 0){
-        snprintf(command, sizeof(command), "%s%s-nom/", mathurl, argv[2]);
+    if (strcmp(argv[1], "math") == 0 && (argc >= 4)){
+        snprintf(command, sizeof(command), "%s%s-item-%s/", mathurl, argv[2], argv[3]);
+        printf("%s\n", command);
     }
-    if (strcmp(argv[1], "rus") == 0){
-        snprintf(command, sizeof(command), "%s%s", rusurl, argv[2]);
+    else if (strcmp(argv[1], "geom") == 0){
+        snprintf(command, sizeof(command), "%s%s/", geomurl, argv[2]);
+        printf("%s\n", command);
     }
-    if (strcmp(argv[1], "en") == 0){
+    else if (strcmp(argv[1], "rus") == 0){
+        snprintf(command, sizeof(command), "%s%s&predmet=ribchenkova10-11", rusurl, argv[2]);
+    }
+    else if (strcmp(argv[1], "en") == 0){
         snprintf(command, sizeof(command), "%smodule%s/%s/1", enurl, argv[2], argv[3]);
     }
+    else return 1;
+
     char *args[] = {"xdg-open", command, NULL};
     execvp(args[0], args);
     return 0;
